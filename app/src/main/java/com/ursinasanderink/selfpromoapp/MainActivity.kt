6d962cee86239836ffe97b16f5f28b526f7e2823
Ursina Sanderink <ursinasanderink@Ursinas-MBP.hsd1.fl.comcast.net>
2022-02-14 10:56:48 -0500

    Initial commit

---
+++ app/src/main/java/com/ursinasanderink/selfpromoapp/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.ursinasanderink.selfpromoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.Spinner
+import android.widget.Toast
+import com.google.android.material.textfield.TextInputEditText
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+        button_preview.setOnClickListener {
+            onPreviewClicked()
+
+        }
+    }
+
+    private fun onPreviewClicked() {
+       val contactName = edit_text_contact_name.text.toString()
+       val contactNumber = edit_text_contact_number.text.toString()
+       val myDisplayName = edit_text_my_display_name.text.toString()
+       val includeJunior = check_box_junior.isChecked
+       val jobTitle = spinner_job_title.selectedItem.toString()
+        val immediateStart = check_box_immediate_start.isChecked
+        val startDate = edit_text_start_date.text.toString()
+
+        val testString = "Hardcoded values here $contactName some more values $contactNumber"
+
+        Toast.makeText(this, "", Toast.LENGTH_LONG).show()
+    }
+}
