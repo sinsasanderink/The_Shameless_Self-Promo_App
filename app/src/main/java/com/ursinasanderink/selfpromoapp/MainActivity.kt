5697fc5780b0500c4727fcc7aba80eb8cf4e0505
sinasanderink <sanderink.ursina@yahoo.com>
2022-02-16 12:58:26 -0500

    .

--- app/src/main/java/com/ursinasanderink/selfpromoapp/MainActivity.kt
+++ app/src/main/java/com/ursinasanderink/selfpromoapp/MainActivity.kt
@@ -1,12 +1,10 @@
 package com.ursinasanderink.selfpromoapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
-import android.widget.CheckBox
-import android.widget.Spinner
-import android.widget.Toast
+import android.widget.*
 import com.google.android.material.textfield.TextInputEditText
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -18,12 +16,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-
         button_preview.setOnClickListener {
             onPreviewClicked()
-
         }
+
+        val spinnerValues: Array<String> = arrayOf("Android Developer", "Android Engineer")
+        val spinnerAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item,spinnerValues)
+        spinner_job_title.adapter = spinnerAdapter
+
     }
 
     private fun onPreviewClicked() {
@@ -31,12 +31,22 @@ class MainActivity : AppCompatActivity() {
        val contactNumber = edit_text_contact_number.text.toString()
        val myDisplayName = edit_text_my_display_name.text.toString()
        val includeJunior = check_box_junior.isChecked
-       val jobTitle = spinner_job_title.selectedItem.toString()
+       val jobTitle = spinner_job_title.selectedItem?.toString()
         val immediateStart = check_box_immediate_start.isChecked
         val startDate = edit_text_start_date.text.toString()
 
-        val testString = "Hardcoded values here $contactName some more values $contactNumber"
+        // In Kotlin, the word New is not needed to make a new object
+        // referring to happens with :: in Kotlin
+       val previewActivityIntent = Intent(this, PreviewActivity::class.java)
+        // If you want any extra data use the putExtra method:
+        previewActivityIntent.putExtra("Contact Name", contactName)
+        previewActivityIntent.putExtra("Contact Number", contactNumber)
+        previewActivityIntent.putExtra("My Display Name", myDisplayName)
+        previewActivityIntent.putExtra("Include Junior", includeJunior)
+        previewActivityIntent.putExtra("Job Title", jobTitle)
+        previewActivityIntent.putExtra("Immediate Start", immediateStart)
+        previewActivityIntent.putExtra("Start Date", startDate)
+        startActivity(previewActivityIntent)
 
-        Toast.makeText(this, "", Toast.LENGTH_LONG).show()
     }
 }
---
+++ app/src/main/java/com/ursinasanderink/selfpromoapp/PreviewActivity.kt
@@ -0,0 +1,23 @@
+package com.ursinasanderink.selfpromoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_preview.*
+
+class PreviewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_preview)
+        val contactName = intent.getStringExtra("Contact Name")
+        val contactNumber = intent.getStringExtra("Contact Number")
+        val myDisplayName = intent.getStringExtra("My Display Name")
+        val includeJunior = intent.getBooleanExtra("Include Junior", false)
+        val jobTitle = intent.getStringExtra("Job Title")
+        val immediateStart = intent.getBooleanExtra("Immediate Start", false)
+        val startDate = intent.getStringExtra("Start Date")
+        val testString = "Contact Name: $contactName, Contact Number: $contactNumber, My Display Name: $myDisplayName, Include Junior: $includeJunior, Job Title: $jobTitle, Immediate Start: $immediateStart, Start Date: $startDate"
+        text_view_message.text = testString
+    }
+
+
+}
